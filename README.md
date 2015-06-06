ff3195f978c8fc5e06f2f0c171612a471f42309a
Peter Reijnders <Peter.Reijnders@verpeteren.nl>
2015-06-06 07:44:30 +0200

    Moved `konstructor.py` and `stylelguidor.py`to here.

--- README.md
+++ README.md
@@ -2,21 +2,73 @@
 
 Several tools that were specially crafted for the nidium project
 
-# Contents
+Contents:
 
-## dokumentor
+ * Konstructor: tool to manage dependencies and build configurations
+ * Styleguidor: tool to rate the c/c++ code
+ * Documentor:	tool to keep the documentation, examples and code in sync.
+
+## Konstructor
+
+Very, and effective way for the developer to keep the dependencies and the build 
+configuration in sync with the gyp settings of the nidium project.
+This uses the :
+ * `deps.py` : module internally to dependenies
+ * `release.py` to create a release
+ * `installer/*` to create installation package
+
+## Show me the code
+
+Every project has a `configure` python script, that uses this.
+
+```
+PYTHONPATH=~/NativeTools/src/&&./configure --third-party=../../third-party/
+```
+
+Basically you pass --third-party the thirdparty path and PYTHONPATH to the path 
+where `konstruktor.py` is
+
+## Styleguidor
+
+Simple, but effective way for the developer to check if the code is more or less
+compliant with the project's styleguide. This is simply a set of regexes, that 
+`rate` and print each line.
+Tool is by no way ideal, but better then nothing.
+
+### Show me the code
+
+You could write a styleguide rule like this:
+
+```
+    'if-(':         {'pattern': '\bif\(', 
+                     #Essential keyword
+                     'text': 'Whitespace needed between if and parenthesis', 
+                     'weight': 0.25},
+```
+
+And by pointing `src/styleguidor.py` to the file's directory it could create:
+
+```
+...
+src/NativeASCII.h:6	Whitespace must follow a comma
+...
+The code got a score of 4.5.
+```
+ 
+## Dokumentor
 
 Simple, but effective way to for the developer to keep the documentation and 
 the code in sync without to much hassle.
 
-### Confusing...
+While programming C/C++ to expose Javascript, you write *currently python code*
+(later probably jsdoc) inside '/*$ comments $*/'. These comments will be extracted 
+and transformed with `documentor.py` into `markdown`
 
-While programming C/C++ to expose Javascript, you write python code in comments.
-While running another python script, markdown is generated
+Note: *This tool is will undergo drastic changes in the _near_ future*.
 
 ### Show me the code
 
-In the c/c++ you can write comments like:
+In the c/c++ code you can write comments like:
 
 ```
 /*$
---
+++ src/styleguidor.py
@@ -0,0 +1,149 @@
+#!/usr/bin/python
+"""
+Styleguidor: Rate c/c++ source files accourding to a styleguide
+
+Simple script that goes over the directorys given as cmd arguments, and rates
+each c/c++ file accourding to some regexes
+"""
+
+import sys, os, re, sre_constants
+
+SCORE = 0.0
+EXTENSIONS = ['.c', '.h', '.cpp']
+IGNORES = ['third-party', '.git', 'build', 'gyp', 'port']
+STYLES = {
+    'indentation':  {'pattern': '^\t+', 
+                    #Flame war bait
+                     'text': 'Indentation should be with spaces not with tabs', 
+                     'weight': 1.00},
+    'line-too-long':{'pattern': '.{132,}', 
+                     #pref: 80, have multiple split windows next to each other
+                     'text': 'Line must not exceed 132 chars', 
+                     'weight': 0.25},
+    'surround-=':   {'pattern': '([^ \t]+[\|\+\-=]=)|([\|\+\-=]=[^ \t\s]+)', 
+                     #Improved readability
+                     'text': 'Spaces must surround =', 
+                     'weight': 1},
+    ';':            {'pattern': ';[a-zA-Z]', 
+                     #Improved readablity
+                     'text': 'Whitespace needed after a ;', 
+                     'weight': 0.25},
+    ';;':           {'pattern': ';;', 
+                     #Clean up is prefferedreadablity
+                     'text': 'Double ;',
+                     'weight': 0.15},
+ 
+    'return-(':     {'pattern': '\breturn\(', 
+                     #Essential keyword
+                     'text': 'Whitespace needed between return and parenthesis', 
+                     'weight': 0.25},
+    'if-(':         {'pattern': '\bif\(', 
+                     #Essential keyword
+                     'text': 'Whitespace needed between if and parenthesis', 
+                     'weight': 0.25},
+    'for-(':        {'pattern': '\bfor\(', 
+                     #Essential keyword
+                     'text': 'Whitespace needed between for and parenthesis', 
+                     'weight': 0.25},
+    'do-(':         {'pattern': '\bdo\(', 
+                     #Essential keyword
+                     'text': 'Whitespace needed between do and parenthesis', 
+                     'weight': 0.25},
+    'switch-(':     {'pattern': '\bswitch\(', 
+                     #Essential keyword
+                    'text': 'Whitespace needed between switch and paranthesis', 
+                     'weight': 0.25},
+    'while-(':      {'pattern': '\bwhile\(', 
+                     #Essential keyword
+                     'text': 'Whitespace needed between while and parenthesis', 
+                     'weight': 0.25},
+    'paren-accu':   {'pattern': '\){', 
+                     #This makes a the end of condition and the start of a block
+                     'text': 'Whitespace needed between ) and {', 
+                     'weight': 0.25},
+    'paren-start':  {'pattern': '\([ \t]', 
+                     #This makes it obvious that the inside belongs together 
+                     'text': 'Whitespace not allowed after a (', 
+                     'weight': 0.25},
+    'paren-end':    {'pattern': '[ \t]\)', 
+                     #This makes it obvious that the inside belongs together 
+                     'text': 'Whitespace not allowed before a )', 
+                     'weight': 0.25},
+    'space-after-comma':{'pattern': ',[^ \t\s]', 
+                     #This improves readablity
+                    'text': 'Whitespace must follow a comma', 
+                    'weight': 0.5},
+    'ends-with-space':{'pattern': '.*[ \t]+$', 
+                     #This looks horrible in pacman mode
+                     'text': 'No whitespace allowed at the end of line', 
+                     'weight': 0.01},
+    #'preproc-indent':{'pattern': '^([ \t]+#)|(#[ \t]+)', 
+                     #Some compilers do not handle this correctly
+    #                'text': 'Indentation not allowed for directive', 
+    #                'weight': 0.01},
+}
+
+def load():
+    "setup the styles to a dict instead of a tuple)"
+    IGNORES.insert(0, '.')
+    for key, style in STYLES.items():
+        try:
+            style["re"] = re.compile(style['pattern'])
+            style['score'] = 0.0 
+        except sre_constants.error:
+            print("Error in regex: " + key + ": '" + style['pattern'] + "'")
+            sys.exit(1)
+
+def usage():
+    """Show the usage."""
+    print("Usage: " + sys.argv[0] + " path1, [path2, ..]")
+    sys.exit()
+
+def process_file(path):
+    "Read the file, process it line by line and show the improvements/rate"
+    global SCORE
+    ext = os.path.splitext(path)[1]
+    if ext in EXTENSIONS:
+        file_hd = open(path, 'r')
+        line = ''
+        line_nr = 0
+        for line_nr, line in enumerate(file_hd.readlines()):
+            for key, style in STYLES.items():
+                result = style['re'].search(line)
+                if result:
+                    print(path + ":" + str(line_nr + 1) + "\t" + style['text'])
+                    SCORE += style['weight']
+                    STYLES[key]['score'] += style['weight']
+        file_hd.close()
+        if line != "\n":
+            print(path + ":" + str(line_nr) + "\tDoes not end with a newline")
+            SCORE += 1
+
+def process_dir(path):
+    "Process path if it is a file, else walk and process recursively"
+    if os.path.isfile(path):
+        process_file(path)
+    else:
+        for root, dirs, files in os.walk(path, topdown=True):
+            for ign in IGNORES:
+                if ign in dirs:
+                    dirs.remove(ign)
+            for name in files:
+                fullpath = os.path.join(root, name)
+                process_file(fullpath)
+
+def show_score():
+    "Display the score"
+    print("\nThe code got a score of " + str(SCORE) + ".")
+
+def main():
+    """Start and run the files/dirs from the command line"""
+    if len(sys.argv) == 1:
+        usage()
+    load()
+    for arg in sys.argv[1::]:
+        process_dir(arg)
+    show_score()
+
+if __name__ == '__main__':
+    main()
