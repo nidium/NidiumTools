4372804a2869059c7c9fd22469b00a8d719aad7c
Peter Reijnders <peter.reijnders@verpeteren.nl>
2016-09-08 07:03:46 +0200

    Simplified Makefile

--- IDL/examples/common.mk
+++ IDL/examples/common.mk
@@ -7,8 +7,19 @@ TEMPLATE_FILES=\
         ../../idl2cpp_templates/base_class.h.tpl \
         ../../idl2cpp_templates/dict_class.h.tpl
 
+$(OUTPUT_DIR)/%.code: $(INPUT_DIR)/%.idl ../../$(PROG).py $(TEMPLATE_FILES) $(CLANG-FORMATSTYLE)
+	@rm -rf $@
+	@$(PYWIDL) -n -o $@ -t $(PROG) $< $(PARAMS)
+	@$(CLANGFORMAT) -i `cat $@`
+
+$(TEMPLATE_FILES): ./templates
+
+./templates:
+	@ln -s ../../idl2cpp_templates templates
+
 .PHONY: clean
 
 clean:
-	rm -rf *.pyc \
+	@rm -rf *.pyc \
+	./templates \
 	$(shell find $(OUTPUT_DIR) -name '*.code' -o -name '*_code')
